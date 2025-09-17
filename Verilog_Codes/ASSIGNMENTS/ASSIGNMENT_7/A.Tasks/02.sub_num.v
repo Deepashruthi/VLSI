ebf0732096aa82f9ce95b1eb4bbef10643825e3f
DEEPASHRUTHI C <146473846+Deepashruthi@users.noreply.github.com>
2025-09-17 17:52:26 +0530

    Update 02.sub_num.v

--- Verilog_Codes/ASSIGNMENTS/ASSIGNMENT_7/A.Tasks/02.sub_num.v
+++ Verilog_Codes/ASSIGNMENTS/ASSIGNMENT_7/A.Tasks/02.sub_num.v
@@ -2,7 +2,7 @@ module sub_num;
   reg [7:0]A,B;
   reg signed [8:0]Sub;
 
-  task sum_task(input [7:0]A,input [7:0]B, output [8:0]out);
+  task sub_task(input [7:0]A,input [7:0]B, output [8:0]out);
     begin
       out=A-B;
     end
@@ -11,15 +11,15 @@ module sub_num;
   initial begin
     $monitor("Time=%t | A=%b(%d) | B=%b(%d)| Sub=%b(%d)" ,$time,A,A,B,B,Sub,Sub);
     A=8'd15; B=8'd45;
-    sum_task(A,B,Sub);
+    sub_task(A,B,Sub);
     #10;A=8'd13; B=8'd12;
-    sum_task(A,B,Sub);
+    sub_task(A,B,Sub);
     #10;A=8'd30; B=8'd30;
-    sum_task(A,B,Sub);
+    sub_task(A,B,Sub);
     #10;A=8'd30; B=8'd15;
-    sum_task(A,B,Sub);
+    sub_task(A,B,Sub);
     #10;A=8'd35; B=8'd15;
-    sum_task(A,B,Sub);
+    sub_task(A,B,Sub);
     #100;
     $finish;
   end
