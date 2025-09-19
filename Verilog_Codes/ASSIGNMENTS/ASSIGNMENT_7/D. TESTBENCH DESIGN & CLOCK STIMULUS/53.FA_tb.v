37539dba54411bf81828636b908c8cbad7e05b6c
DEEPASHRUTHI C <146473846+Deepashruthi@users.noreply.github.com>
2025-09-19 14:14:45 +0530

    Add files via upload

---
+++ Verilog_Codes/ASSIGNMENTS/ASSIGNMENT_7/D. TESTBENCH DESIGN & CLOCK STIMULUS/53.FA_tb.v
@@ -0,0 +1,17 @@
+module fa_tb;
+  reg a, b, cin;
+  wire sum, cout;
+
+  full_adder uut (.a(a), .b(b), .cin(cin), .sum(sum), .cout(cout));
+
+  integer i;
+  initial begin
+    $display("a b cin : sum cout");
+    for(i=0;i<8;i=i+1) begin
+      {a,b,cin} = i; #10;
+      $display("%b %b %b : %b %b", a, b, cin, sum, cout);
+    end
+    $finish;
+  end
+endmodule
+
